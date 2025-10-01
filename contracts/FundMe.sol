3b710c9832cd7e947f723109576e6c64060c62b9
anmolkhurana5205 <anmol.2327cse1235@kiet.edu>
2025-10-01 21:21:48 +0530

    '#addedWithdrawFunction'

--- contracts/FundMe.sol
+++ contracts/FundMe.sol
@@ -12,8 +12,15 @@ contract FundMe {
     using PriceConverter for uint256;
 
     uint256 public minimumUsd = 50*1e18;
+
     address[] public funders;
     mapping (address => uint256) public addressToAmountFunded;
+
+    address public owner;
+
+    constructor() {
+        owner = msg.sender;
+    }
  
     function fund() public payable {
         // Want to be able to set a minimum fund amount in USD
@@ -22,10 +29,27 @@ contract FundMe {
         funders.push(msg.sender);
         addressToAmountFunded[msg.sender] += msg.value;
     }
-    function withdraw() public {
+    function withdraw() public OnlyOwner {
         for(uint256 funderIndex = 0; funderIndex < funders.length; funderIndex++) {
             address funder = funders[funderIndex];
-            
+            addressToAmountFunded[funder] = 0;
         }
+        // resetting the array
+        funders = new address[](0);
+
+        // // transfer
+        // payable(msg.sender).transfer(address(this).balance);
+        // // send
+        // bool sendSuccess = payable(msg.sender).send(address(this).balance);
+        // require(sendSuccess, "Send Failed");
+        // // call
+
+        (bool callSuccess, ) = payable(msg.sender).call{value: address(this).balance}("");
+        require(callSuccess, "Call Failed");
+    }
+
+    modifier OnlyOwner() {
+        require(msg.sender == owner, "Sender is not owner!");
+        _;
     }
 }
